e4cb73c5dce3be90179481bba99593158cee5bd9
Aidan Bleser <aidanbleser35@gmail.com>
2024-11-10 08:02:43 -0600

    Fix config files

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
